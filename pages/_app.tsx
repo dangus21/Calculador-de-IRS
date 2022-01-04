0c144ab0c118ba9ad7ee5717ea1163d56bbe7c94
angus <angusmiguel21@gmail.com>
2022-01-04 23:47:06 +0000

    Page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import "tailwindcss/tailwind.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
-
+import Head from "next/head"
 const colors = {};
 
 const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Head>
+      <title>Calculador De IRS</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Head>
   )
 }
 
